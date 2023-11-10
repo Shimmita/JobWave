fe85fe0588bf6f286dd1831ded5086795b7acf06
Shimmita <douglasshimita3@gmail.com>
2023-11-10 15:30:26 +0300

    stand point error on link as param in nav router

---
+++ app/src/main/java/com/shimitadouglas/jobwave/routes/Routes.kt
@@ -0,0 +1,8 @@
+package com.shimitadouglas.jobwave.routes
+
+object routes {
+    const val SCREEN_SPLASH = "SPLASH"
+    const val SCREEN_HOME = "HOME"
+    const val SCREEN_JOB = "JOBS"
+    const val SCREEN_JOB_APPLICATION="JOB_APPLICATION"
+}
--- app/src/main/java/com/shimitadouglas/jobwave/routes/routes.kt
+++
@@ -1,7 +0,0 @@
-package com.shimitadouglas.jobwave.routes
-
-object routes {
-    val SCREEN_SPLASH = "SPLASH"
-    val SCREEN_HOME = "HOME"
-    val SCREEN_JOB = "JOBS"
-}
